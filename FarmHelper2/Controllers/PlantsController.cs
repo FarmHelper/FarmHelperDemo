b356e9239aaa7ac27585e7263b6257286d1e8f64
squis <squishymaster12@gmail.com>
2016-12-10 18:53:26 +0200

    WebAPI first implementation

---
+++ FarmHelper2/Controllers/PlantsController.cs
@@ -0,0 +1,20 @@
+﻿using FarmHelper2.Backend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FarmHelper2.Controllers
+{
+    public class PlantsController : ApiController
+    {
+
+        public IHttpActionResult Post(Parcela plant)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ FarmHelper2/FarmApp/Scripts/landingController.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict'
+
+    angular
+        .module('FarmApp')
+        .controller('LandingController', LandingController)
+
+    LandingController.$inject = ['$location', 'httpService'];
+
+    function LandingController(location, httpService) {
+
+        var vm = this;
+        vm.getPlants = getPlants;
+
+        function getPlants() {
+            alert('Hello')
+            vm.data = {}
+            vm.data.umiditate = 3
+            httpService.post('api/Plants', vm.data);
+        }
+
+    }
+})();
