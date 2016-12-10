c1a8e9048ae146b5aa2254d2f8ae9b56da518e6f
squis <squishymaster12@gmail.com>
2016-12-10 16:52:17 +0200

    LandingPage

---
+++ FarmHelper2/FarmApp/app.module.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict'
+
+    angular
+        .module('FarmApp', ['ngRoute', 'ngAnimate', 'ngAria', 'ngMaterial'])
+        .config(function ($routeProvider) {
+            $routeProvider
+                .when("/landing", {
+                    templateUrl: "FarmApp/Views/LandingPage.html"
+                })
+                .when("/exams", {
+                    templateUrl: "Template/Exams"
+                })
+                .when("/questions", {
+                    templateUrl: "Template/Questions"
+                })
+                .when("/sections", {
+                    templateUrl: "Template/Sections"
+                })
+                .when("/administrators", {
+                    templateUrl: "Template/Administrators"
+                })
+		        .otherwise({ redirectTo: "/landing" });
+        });
+})();
