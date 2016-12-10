471059cfd4f363f9da688bcb88bd308ec1d8c00f
squis <squishymaster12@gmail.com>
2016-12-10 17:14:15 +0200

    Fixing Publish

--- FarmHelper2/App_Start/BundleConfig.cs
+++ FarmHelper2/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace FarmHelper2
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/farmapp").Include(
+                      "~/FarmApp/app.module.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
